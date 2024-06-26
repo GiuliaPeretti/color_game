ce6a7d083497aa26d2fb89673b54f801f7593387
GiuliaPeretti <giulia.peretti12@gmail.com>
2024-06-26 16:41:28 +0200

    working on input

--- main.py
+++ main.py
@@ -43,19 +43,36 @@ def draw_color_bar():
         colors.append([x,y,w,w])
         pygame.draw.rect(screen, i, (x,y,w,w), border_radius=10)
         pygame.draw.rect(screen, WHITE, (x,y,w,w), 3, 10)
-        x=x+w+10
+        x=x+w-2
 
 def draw_squares(n_per_row=3):
+    matrix=[]
 
-    w=3*(560-90)/4
-    x_offset=90
-    y_offset=50
-    x,y=x_offset,SCREEN_HEIGHT-y_offset-w
+    if(n_per_row/(70*6)<(SCREEN_HEIGHT-170)/5):
+        w=(SCREEN_HEIGHT-170)/5
+    else:
+        w=(70*6)/n_per_row
+    print(w)
+    x_offset=(560-90)/4/(n_per_row-1)
+    x_offset=-2
+    y_offset=-2
+    x,y=90+(70*6-w*n_per_row)/2,30
     for row in range(5):
-        x=x_offset,SCREEN_HEIGHT-y_offset-w
+        temp=[]
+        x=90+(70*6-w*n_per_row)/2
         for col in range(n_per_row):
-            pygame.draw.rect(screen, WHITE, (100,100,50,50), 3, 10)
-            x=x+w+10
+            pygame.draw.rect(screen, WHITE, (x,y,w,w), 3, 10)
+            temp.append([x,y,w,w])
+            x=x+w+x_offset
+        matrix.append(temp)
+        y=y+w+y_offset
+        return(matrix)
+
+def gen_puzzle(n_per_row=3):
+    puzzle=[]
+    for i in range(n_per_row):
+        puzzle.append(random.choice(COLORS))
+    return(puzzle)
 
 
 
@@ -69,10 +86,14 @@ if __name__=='__main__':
     pygame.display.set_caption('Campo minato♥')
     font = pygame.font.SysFont('arial', 20)
 
+    win=False
+    loose=False
     selected=-1
+    game_started=False
     buttons=gen_buttons()
     draw_buttons()
     draw_color_bar()
+    matrix=draw_squares()
 
     run  = True
     number=0
@@ -84,6 +105,32 @@ if __name__=='__main__':
                 run = False
             if event.type == pygame.MOUSEBUTTONDOWN:
                 x,y=pygame.mouse.get_pos()
+
+                for i in range (len(buttons)):
+                    if(x>=buttons[i]['coordinates'][0] and x<=buttons[i]['coordinates'][0]+buttons[i]['coordinates'][2] and y>=buttons[i]['coordinates'][1] and y<=buttons[i]['coordinates'][1]+buttons[i]['coordinates'][3]):
+                        if(i<len(buttons)-1):
+                            selected=i
+                            if(not(game_started)):
+                                select_game=selected
+                            draw_buttons()
+                            break
+                        elif(i==4):
+                            if(selected!=-1):
+                                pygame.draw.rect(screen, BACKGROUND_COLOR, (640,490,100,100))
+                                select_game=selected
+                                selected=-1
+                                win=False
+                                loose=False
+                                game_started=True
+                                first_cell=True
+                                buttons=gen_buttons()
+                                draw_buttons()
+                            break
+                else:
+                    selected=-1
+                draw_buttons()
+
+
                     
 
             if (event.type == pygame.KEYDOWN):
